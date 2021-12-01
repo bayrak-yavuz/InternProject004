0485f201efe9b8e5a23b905a8c22afe106da2274
sefattila <67368585+sefattila@users.noreply.github.com>
2021-12-01 11:41:31 +0300

    sdsadsa

--- lib/kayitol.dart
+++ lib/kayitol.dart
@@ -31,7 +31,7 @@ class _kayitSayfasiState extends State<kayitSayfasi> {
     var size = MediaQuery.of(context).size;
     return Scaffold(
         appBar: AppBar(
-          title: const Text("Kayıt Ol"),
+          title: const Text("Kayıtttt Ol"),
         ),
         body: Center(
           child: Padding(
