10d317d1ade4f9d8a8e2ae5cb50ab2497a51be9d
sefattila <67368585+sefattila@users.noreply.github.com>
2021-12-01 11:16:46 +0300

    ss

--- lib/kayitol.dart
+++ lib/kayitol.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, camel_case_types
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
