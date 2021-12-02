baa80ae376553011c883992cc4dfe0f57cee4a7e
sefattila <67368585+sefattila@users.noreply.github.com>
2021-12-02 12:10:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -112,99 +112,104 @@ class _IskeleState extends State<Iskele> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey.shade800.withOpacity(.75),
+          title: Text("toDoList"),
+          centerTitle: true,
+        ),
         body: Center(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10.0),
-        child: Container(
-          height: size.height * .5,
-          width: size.width * .85,
-          decoration: BoxDecoration(
-              color: Colors.blueGrey.shade800.withOpacity(.75),
-              borderRadius: BorderRadius.all(Radius.circular(20)),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(.75),
-                  blurRadius: 10,
-                  spreadRadius: 2,
-                )
-              ]),
           child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    controller: t1,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    cursorColor: Colors.white,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.mail,
-                        color: Colors.white,
-                      ),
-                      hintText: 'E-Posta',
-                      prefixText: ' ',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      focusColor: Colors.white,
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: Container(
+              height: size.height * .5,
+              width: size.width * .85,
+              decoration: BoxDecoration(
+                  color: Colors.blueGrey.shade800.withOpacity(.75),
+                  borderRadius: BorderRadius.all(Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(.75),
+                      blurRadius: 10,
+                      spreadRadius: 2,
+                    )
+                  ]),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        controller: t1,
+                        style: TextStyle(
                           color: Colors.white,
                         ),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
+                        cursorColor: Colors.white,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.mail,
+                            color: Colors.white,
+                          ),
+                          hintText: 'E-Posta',
+                          prefixText: ' ',
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          focusColor: Colors.white,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.02,
-                  ),
-                  TextFormField(
-                    controller: t2,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    cursorColor: Colors.white,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(
-                        Icons.vpn_key,
-                        color: Colors.white,
+                      SizedBox(
+                        height: size.height * 0.02,
                       ),
-                      hintText: 'Parola',
-                      prefixText: ' ',
-                      hintStyle: TextStyle(
-                        color: Colors.white,
-                      ),
-                      focusColor: Colors.white,
-                      focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
+                      TextFormField(
+                        controller: t2,
+                        style: TextStyle(
                           color: Colors.white,
                         ),
-                      ),
-                      enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(
-                          color: Colors.white,
+                        cursorColor: Colors.white,
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.vpn_key,
+                            color: Colors.white,
+                          ),
+                          hintText: 'Parola',
+                          prefixText: ' ',
+                          hintStyle: TextStyle(
+                            color: Colors.white,
+                          ),
+                          focusColor: Colors.white,
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.02,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      girisYap();
+                      SizedBox(
+                        height: size.height * 0.02,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          girisYap();
 
-                      /*isValid = EmailValidator.validate(t1.text);
+                          /*isValid = EmailValidator.validate(t1.text);
                       if (isValid) {
                         girisYap();
                       } else if (t1.text.isEmpty) {
@@ -222,95 +227,98 @@ class _IskeleState extends State<Iskele> {
                             timeInSecForIosWeb: 1,
                             fontSize: 16.0);
                       }*/
-                    },
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 5),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white, width: 2),
-                          borderRadius: BorderRadius.all(Radius.circular(30))),
-                      child: Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: Center(
-                          child: Text(
-                            "Giriş Yap",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 20,
+                        },
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          decoration: BoxDecoration(
+                              border: Border.all(color: Colors.white, width: 2),
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(30))),
+                          child: Padding(
+                            padding: const EdgeInsets.all(5.0),
+                            child: Center(
+                              child: Text(
+                                "Giriş Yap",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20,
+                                ),
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.02,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      FirebaseAuth.instance.signOut().then((deger) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (_) => sifremiUnuttum()),
+                      SizedBox(
+                        height: size.height * 0.02,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          FirebaseAuth.instance.signOut().then((deger) {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (_) => sifremiUnuttum()),
                             );
-                      });
-                      //şifremi unuttum
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(
-                          "Şifremi Unuttum",
-                          style: TextStyle(color: Colors.white),
+                          });
+                          //şifremi unuttum
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text(
+                              "Şifremi Unuttum",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: size.height * 0.06,
-                  ),
-                  InkWell(
-                    onTap: () {
-                      FirebaseAuth.instance.signOut().then((deger) {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (_) => kayitSayfasi(),
-                            ));
-                      });
-                      //kayitOl();
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Container(
-                          height: 4,
-                          width: 75,
-                          color: Colors.white,
+                      ),
+                      SizedBox(
+                        height: size.height * 0.06,
+                      ),
+                      InkWell(
+                        onTap: () {
+                          FirebaseAuth.instance.signOut().then((deger) {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (_) => kayitSayfasi(),
+                                ));
+                          });
+                          //kayitOl();
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Container(
+                              height: 4,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Kayıt Ol",
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 25),
+                            ),
+                            Container(
+                              height: 4,
+                              width: 75,
+                              color: Colors.white,
+                            ),
+                          ],
                         ),
-                        Text(
-                          "Kayıt Ol",
-                          style: TextStyle(color: Colors.white, fontSize: 25),
-                        ),
-                        Container(
-                          height: 4,
-                          width: 75,
-                          color: Colors.white,
-                        ),
-                      ],
-                    ),
-                  ),
-                  /*Row(
+                      ),
+                      /*Row(
                       children: [
                         ElevatedButton(child: Text("Kaydol"), onPressed: kayitOl),
                         ElevatedButton(child: Text("Giriş Yap"), onPressed: girisYap),
                       ],
                     ),*/
-                ],
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
